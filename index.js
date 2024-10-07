0858e0cbf89ae4a9b52e1d79f741c723ada5469e
alanesg <alanesGsilva@gmail.com>
2024-10-07 12:23:16 -0300

    primeira alteração

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-//
+
+
+
+//função e estrutura de decisão
+function calcular(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas
+    let nivelHeroi
+
+    {
+        if (saldoVitorias < 10) {
+            nivelHeroi = " FERRO!"
+        } else if (saldoVitorias >= 11 && saldoVitorias <= 20) {
+            nivelHeroi = " BRONZE!"
+        } else if (saldoVitorias >= 21 && saldoVitorias <= 50) {
+            nivelHeroi = "PRATA!"
+        } else if (saldoVitorias >= 51 && saldoVitorias <= 80) {
+            nivelHeroi = "OURO!"
+        } else if (saldoVitorias >= 81 && saldoVitorias <= 90) {
+            nivelHeroi = "DIAMANTE!"
+        } else if (saldoVitorias >= 91 && saldoVitorias <= 100) {
+            nivelHeroi = "LENDÁRIO!"
+        } else {
+            nivelHeroi = "IMORTAL!"
+        }
+
+        return {saldoVitorias, nivelHeroi}
+       
+    }
+
+}
+    
+ while (true){
+    let vitorias =( 80)
+  let derrotas = ( 25)
+
+    let resultado = calcular (vitorias, derrotas)
+    console.log(`O Herói tem o saldo de: ${resultado.saldoVitorias} e está no nível: ${resultado.nivelHeroi}`);
+
+    break
+ }
+
